e567879071d64faad3637b77b1a125cab3c95b33
solomonade1 <solomonadegboyega1@gmail.com>
2023-09-16 22:20:18 +0100

    task added

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -18,7 +18,7 @@ const Featured = async () => {
   );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+    throw new Error("Failed to fetch data, Featured error"); // this will be caught by the error page and passed to the page as props
   }
 
   const data = await res.json();
